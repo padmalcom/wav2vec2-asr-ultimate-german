fe4496622ceadd0904427a4c131765d73a2b258c
padmalcom <j.freiknecht@googlemail.com>
2023-02-13 17:20:36 +0100

    Fixed tuple issue on labels.

--- train.py
+++ train.py
@@ -102,7 +102,7 @@ class DataCollatorCTCWithPadding:
 class CTCTrainer(Trainer):
 	def _prepare_inputs(self, inputs):
 		for k, v in inputs.items():
-			print("Key:", k, "value:", v)
+			#print("Key:", k, "value:", v)
 			if isinstance(v, torch.Tensor):
 				kwargs = dict(device=self.args.device)
 				if self.deepspeed and inputs[k].dtype != torch.int64:
@@ -110,11 +110,17 @@ class CTCTrainer(Trainer):
 				inputs[k] = v.to(**kwargs)
 
 			if k == 'labels': # labels are list of tensor, not tensor, special handle here
+				#inputs[k] = inputs[k].to(**kwargs)
+				new_labels = []
 				for i in range(len(inputs[k])):
-					kwargs = dict(device=self.args.device)
-					if self.deepspeed and inputs[k][i].dtype != torch.int64:
-						kwargs.update(dict(dtype=self.args.hf_deepspeed_config.dtype()))
-					inputs[k][i] = inputs[k][i].to(**kwargs)
+				#	kwargs = dict(device=self.args.device)
+					#print("Input: ", inputs[k][i], "type: ", type(inputs[k][i]))
+				#	if self.deepspeed and inputs[k][i].dtype != torch.int64:
+				#		kwargs.update(dict(dtype=self.args.hf_deepspeed_config.dtype()))
+				#	inputs[k][i] = inputs[k][i].to(**kwargs)
+					new_labels.append(inputs[k][i].to(**kwargs))
+				inputs[k] = tuple(new_labels)
+				
 
 		if self.args.past_index >= 0 and self._past is not None:
 			inputs["mems"] = self._past
@@ -143,11 +149,7 @@ class CTCTrainer(Trainer):
 		model.train()
 		inputs = self._prepare_inputs(inputs)
 
-		if self.use_amp:
-			with autocast():
-				loss = self.compute_loss(model, inputs)
-		else:
-			loss = self.compute_loss(model, inputs)
+		loss = self.compute_loss(model, inputs)
 
 		if self.args.n_gpu > 1:
 			loss = loss.mean()
@@ -155,9 +157,7 @@ class CTCTrainer(Trainer):
 		if self.args.gradient_accumulation_steps > 1:
 			loss = loss / self.args.gradient_accumulation_steps
 
-		if self.use_amp:
-			self.scaler.scale(loss).backward()
-		elif self.use_apex:
+		if self.use_apex:
 			with amp.scale_loss(loss, self.optimizer) as scaled_loss:
 				scaled_loss.backward()
 		elif self.deepspeed:
