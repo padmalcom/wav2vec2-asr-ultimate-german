1ef60b066ce57e6459442f7f5b562fb853cb7e52
padmalcom <j.freiknecht@googlemail.com>
2023-03-22 16:57:39 +0100

    balanced training data.

---
+++ preprocess_balance.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import os
+
+TRAIN_FILE = os.path.join("common-voice-12", "train.csv")
+BALANCED_TRAIN_FILE = os.path.join("common-voice-12", "train_balanced.csv")
+
+if __name__ == '__main__':
+	df = pd.read_csv(TRAIN_FILE)
+	age_group = df.groupby(['age'])
+	df_age_count = age_group.count()
+	min_age = df_age_count['file'].min()
+	print("Min group samples:", min_age)
+
+	#balanced = age_group.apply(lambda x: x.sample(age_group.size().min()).reset_index(drop=True))
+	balanced = age_group.sample(min_age).reset_index(drop=True)
+	print("Balanced groups (head):", balanced.head(20))
+	
+	balanced.to_csv(BALANCED_TRAIN_FILE, index=False)
+	
+	
